f23980bdc954237961c0be8c09eb3f60747e6ee6
Garry Michael Pereira <garrymp12@gmail.com>
2023-11-16 21:35:37 -0600

    implementing game struct incomplete

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
 use std::io;
 
 fn main() {
-    let game: Game = Game::new(String::from("potato"));
+    let mut game: Game = Game::new(String::from("potato"));
     println!(
         "word: {}, max guesses: {}, lives: {}",
         game.word, game.letters_left_to_guess, game.lives
     );
 
     // get player guess
-    let mut player_input = String::new();
-    let guess: &str = match io::stdin().read_line(&mut player_input) {
-        Ok(_) => {
-            let output: &str = player_input.trim();
-            output
-        }
-        Err(error) => panic!("{} was the error", error),
-    };
+    let mut current_guess: String = String::new();
+    current_guess = guess(&mut current_guess);
 
     // compare the guess to the word
-    if game.word.contains(guess) {
-        println!("the word: {} contains {}", game.word, guess);
-    } else {
-        println!("the word: {} does not contain {}", game.word, guess);
-    }
+    game.processing(current_guess);
 }
 
 struct Game {
     word: String,
     letters_left_to_guess: u8,
     lives: u8,
+    player_guess: String,
 }
 
 impl Game {
@@ -38,6 +29,26 @@ impl Game {
             word,
             letters_left_to_guess: length,
             lives: 6,
+            player_guess: String::new(),
         }
     }
+
+    fn processing(&self, guess: String) {
+        if self.word.contains(guess) {
+            println!("the word: {} contains {}", self.word, guess);
+        } else {
+            println!("the word: {} does not contain {}", self.word, guess);
+        }
+    }
+}
+
+fn guess(player_guess: &mut String) -> &str {
+    let guess: &str = match io::stdin().read_line(player_guess) {
+        Ok(_) => {
+            let output: &str = player_guess.trim();
+            output
+        }
+        Err(error) => panic!("{} was the error", error),
+    };
+    guess
 }
