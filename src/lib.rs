e42233e2b405d3c26ee3d303392aca673670ef49
Garry Michael Pereira <garrymp12@gmail.com>
2023-11-18 21:09:47 -0600

    created associated functions guess check and new

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+use std::io;
+
+pub struct Game {
+    word: String,
+    letters_left_to_guess: u8,
+    lives: u8,
+}
+
+impl Game {
+    pub fn new(word: String) -> Game {
+        let length: u8 = word.len() as u8;
+        Game {
+            word,
+            letters_left_to_guess: length,
+            lives: 6,
+        }
+    }
+
+    pub fn guess(guess: &mut String) -> &str {
+        let guess_output: &str = match io::stdin().read_line(guess) {
+            Ok(_) => {
+                let output: &str = guess.trim();
+                output
+            }
+            Err(error) => panic!("{} was the error", error),
+        };
+        guess_output
+    }
+
+    pub fn check(&self, guess: &str) {
+        if self.word.contains(guess) {
+            println!("word: {} contains guess: {}", self.word, guess);
+        } else {
+            println!("word: {} does NOT contain guess: {} ", self.word, guess);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,11 @@
-use std::io;
-
+use hangman::Game;
 fn main() {
-    let mut game: Game = Game::new(String::from("potato"));
-    println!(
-        "word: {}, max guesses: {}, lives: {}",
-        game.word, game.letters_left_to_guess, game.lives
-    );
+    let game: Game = Game::new(String::from("potato"));
 
     // get player guess
-    let mut current_guess: String = String::new();
-    current_guess = guess(&mut current_guess);
+    let mut guess: String = String::new();
+    let guess_to_handle = Game::guess(&mut guess);
 
     // compare the guess to the word
-    game.processing(current_guess);
-}
-
-struct Game {
-    word: String,
-    letters_left_to_guess: u8,
-    lives: u8,
-    player_guess: String,
-}
-
-impl Game {
-    fn new(word: String) -> Game {
-        let length: u8 = word.len() as u8;
-        Game {
-            word,
-            letters_left_to_guess: length,
-            lives: 6,
-            player_guess: String::new(),
-        }
-    }
-
-    fn processing(&self, guess: String) {
-        if self.word.contains(guess) {
-            println!("the word: {} contains {}", self.word, guess);
-        } else {
-            println!("the word: {} does not contain {}", self.word, guess);
-        }
-    }
-}
-
-fn guess(player_guess: &mut String) -> &str {
-    let guess: &str = match io::stdin().read_line(player_guess) {
-        Ok(_) => {
-            let output: &str = player_guess.trim();
-            output
-        }
-        Err(error) => panic!("{} was the error", error),
-    };
-    guess
+    game.check(guess_to_handle);
 }
